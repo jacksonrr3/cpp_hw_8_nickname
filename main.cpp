44e4d75d3f9412bb18e764cb82b7e834331f12fd
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2019-12-18 17:29:15 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "radix_tree.h"
+
+int main() {
+	RadixTrie trie;
+	std::string name;
+	while (std::getline(std::cin, name)) {
+		trie.append(name);
+	}
+	
+	trie.print_names();
+	trie.print_tree();
+
+	return 0;
+}
